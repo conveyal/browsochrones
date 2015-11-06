f13032d9ae7fc08f89536155483ba7b49ecb10e1
Trevor Gerhardt <trevorgerhardt@gmail.com>
2015-11-06 20:11:14 +0700

    Use promises/callbacks appropriately

--- index.js
+++ index.js
@@ -2,57 +2,56 @@
  * Create static travel time images from static site output.
  */
 
-import request from 'browser-request'
 import fetch from 'isomorphic-fetch'
 
 import getSurface from './get-surface'
 import accessibilityForCutoff from './accessibility-for-cutoff'
 import isochroneTile from './isochrone-tile'
 
-function getQuery (url, cb) {
-  request({url: `${url}query.json`, gzip: true}, (err, data, body) => {
-    if (err) console.error(err)
-    cb(JSON.parse(body))
-  })
+function getQuery (url) {
+  return fetch(`${url}query.json`).then(res => res.json())
 }
 
-function getStopTrees (url, cb) {
-  fetch(`${url}stop_trees.dat`).then(res => res.arrayBuffer())
-    .then(res => {
-      let buf = new Int32Array(res)
-      console.log(`Stop trees ${Math.round(buf.byteLength / 1000)}kb uncompressed`)
-      cb(buf)
+function getStopTrees (url) {
+  return fetch(`${url}stop_trees.dat`)
+    .then(res => res.arrayBuffer())
+    .then(ab => new Int32Array(ab))
+    .then(sta => {
+      console.log(`Stop trees ${Math.round(sta.byteLength / 1000)}kb uncompressed`)
+      return sta
     })
 }
 
 /** x, y relative to query origin */
-function getOrigin (url, x, y, cb) {
+function getOrigin (url, x, y) {
   x |= 0 // round off, coerce to integer
   y |= 0
-  fetch(`${url}${x}/${y}.dat`).then(res => res.arrayBuffer())
-    .then(res => {
-      let buf = new Int32Array(res)
-      console.log(`Origin ${Math.round(buf.byteLength / 1000)}kb uncompressed`)
-      cb(buf)
+  return fetch(`${url}${x}/${y}.dat`)
+    .then(res => res.arrayBuffer())
+    .then(ab => new Int32Array(ab))
+    .then(origin => {
+      console.log(`Origin ${Math.round(origin.byteLength / 1000)}kb uncompressed`)
+      return origin
     })
 }
 
 /** download a grid */
-function getGrid (url, category, cb) {
-  fetch(`${url}grids/${category}.grid`).then(res => res.arrayBuffer())
+function getGrid (url, category) {
+  return fetch(`${url}grids/${category}.grid`)
+    .then(res => res.arrayBuffer())
     .then(res => {
-      console.log(`Grid ${res.length / 1000}kb uncompressed`)
-
       // de-delta-code
       // skip header in data
       let arr = new Float64Array(res, 24)
+
+      console.log(`Grid ${arr.byteLength / 1000}kb uncompressed`)
 
       for (let i = 0, prev = 0; i < arr.length; i++) {
         arr[i] = (prev += arr[i])
       }
 
       let dv = new DataView(res)
-      cb({
+      return {
         // parse header
         zoom: dv.getInt32(0, true),
         west: dv.getInt32(4, true),
@@ -60,7 +59,7 @@ function getGrid (url, category, cb) {
         width: dv.getInt32(12, true),
         height: dv.getInt32(16, true),
         data: arr
-      })
+      }
     })
 }
 
@@ -68,19 +67,25 @@ const baseUrl = 'http://s3.amazonaws.com/analyst-static/indy-baseline/'
 
 // retrieve query and stop tree cache
 let query
-getQuery(baseUrl, function (data) {
-  query = data
-})
+getQuery(baseUrl)
+  .then(data => {
+    query = data
+  })
+  .catch(err => console.error(err))
 
 let stopTreeCache
-getStopTrees(baseUrl, function (data) {
-  stopTreeCache = data
-})
+getStopTrees(baseUrl)
+  .then(data => {
+    stopTreeCache = data
+  })
+  .catch(err => console.error(err))
 
 let grid
-getGrid(baseUrl, 'Jobs_total', function (data) {
-  grid = data
-})
+getGrid(baseUrl, 'Jobs_total')
+  .then(data => {
+    grid = data
+  })
+  .catch(err => console.error(err))
 
 let isoLayer = null
 
@@ -106,24 +111,26 @@ map.on('click', function (e) {
 
   if (x < 0 || x > query.width || y < 0 || y > query.height) return // todo should show blank layer
 
-  getOrigin(baseUrl, x, y, function (origin) {
-    console.time('surface')
-    let surface = getSurface(query, stopTreeCache, origin, x, y, 'AVERAGE', grid)
-    console.timeEnd('surface')
+  getOrigin(baseUrl, x, y)
+    .then(origin => {
+      console.time('surface')
+      let surface = getSurface(query, stopTreeCache, origin, x, y, 'AVERAGE', grid)
+      console.timeEnd('surface')
 
-    console.time('accessibility')
-    let access = accessibilityForCutoff(surface, 60, 'AVERAGE')
-    console.timeEnd('accessibility')
+      console.time('accessibility')
+      let access = accessibilityForCutoff(surface, 60, 'AVERAGE')
+      console.timeEnd('accessibility')
 
-    document.querySelector('#access output').value = access
+      document.querySelector('#access output').value = access
 
-    if (isoLayer != null) map.removeLayer(isoLayer)
+      if (isoLayer != null) map.removeLayer(isoLayer)
 
-    isoLayer = window.L.tileLayer.canvas()
-    isoLayer.drawTile = function (canvas, tilePoint, zoom) {
-      isochroneTile(canvas, tilePoint, zoom, query, surface, 60)
-    }
+      isoLayer = window.L.tileLayer.canvas()
+      isoLayer.drawTile = function (canvas, tilePoint, zoom) {
+        isochroneTile(canvas, tilePoint, zoom, query, surface, 60)
+      }
 
-    isoLayer.addTo(map)
-  })
+      isoLayer.addTo(map)
+    })
+    .catch(err => console.error(err))
 })
